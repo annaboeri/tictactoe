d2776f7fab29fe0325e134b41abd618d1a0a5737
Anna Boeri <annaboeri@gmail.com>
2018-01-29 14:20:49 -0800

    updated game logic

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// create game object
 
 
 var player1 = 'X';
@@ -6,8 +5,6 @@ var player2 = 'O';
 var turn = 0;
 
 var squares = document.getElementsByClassName('square');
-
-console.log('Squares', squares);
 
 for (var i = 0; i < squares.length; i++){
   squares[i].addEventListener("click", renderPlayer);
@@ -20,19 +17,20 @@ function renderPlayer(){
     if (turn === 1){
       console.log('turn 1');
       this.innerHTML = player1;
-      this.removeEventListener("click");
+      this.removeEventListener("click", renderPlayer);
     }
     else if (turn % 2 === 0){
       console.log('turn even');
       this.innerHTML = player2;
+      this.removeEventListener("click", renderPlayer);
     }
     else {
       console.log('turn odd');
       this.innerHTML = player1;
+      this.removeEventListener("click", renderPlayer);
     };
   };
 
-// make already clicked squares unclickable 
 
 //Score/winning combos - state of win/state of draw/loss
 
